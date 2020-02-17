9b41f337716a3b1ff29f6168a16ceb60373ad7b0
Simo Khouili <m.simokhouili@gmail.com>
2020-02-17 20:38:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+  end
+
+def batch_badge_creator(attendees)
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+attendees.each{|name| "Hello, my name is #{name}."}
+end
