991b37183680dcc31fe9afafdedbf028f6105b63
Simo Khouili <m.simokhouili@gmail.com>
2020-02-17 20:52:15 +0000

    Automatically backed up by Learn

---
+++ conference_badges.rb
@@ -0,0 +1,16 @@
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+  end
+
+def batch_badge_creator(attendees)
+new_array = []
+array.each {|name| new_array << "Hello, my name is #{name}."}
+return new_array
+end
+# nuarray = []
+#  array.each do |name|
+#    nuarray.push("Hello, my name is #{name}.")
+#  end
+#  return nuarray
+# end
