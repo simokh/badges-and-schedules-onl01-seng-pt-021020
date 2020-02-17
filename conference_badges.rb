67ea0f411a377d6f7d47118a75055c6e175cd033
Simo Khouili <m.simokhouili@gmail.com>
2020-02-17 20:59:33 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,16 @@ array.each do |name|
   end
   return new_array
 end
+
+def room_assignments(array)
+  new_array = []
+  count = 1
+  array.each do |name|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{count}!")
+  end
+  return new_array
+end 
+
 # nuarray = []
 #  array.each do |name|
 #    nuarray.push("Hello, my name is #{name}.")
